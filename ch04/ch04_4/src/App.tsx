30852c0a26634fd6bed61d9d7dcb2e4606567d31
kimJongWan <kimdeiz@naver.com>
2024-03-04 22:26:08 +0900

    learned useEffect that is react hook for life cycle

---
+++ ch04/ch04_4/src/App.tsx
@@ -0,0 +1,13 @@
+import ClassLifecycle from './pages/ClassLifecycle'
+import FetchTest from './pages/FetchTest'
+import WindowResize from './pages/WindowResizeTest'
+
+export default function App() {
+  return (
+    <main>
+      <FetchTest />
+      <WindowResize />
+      <ClassLifecycle />
+    </main>
+  )
+}
