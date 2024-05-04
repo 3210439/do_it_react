2a6f74e232dcf586d3cbd395ce09f17ebaf91159
kimJongWan <kimdeiz@naver.com>
2024-05-04 22:07:47 +0900

    finished learning about ch7_3_server part

--- ch07/ch07_3_server/src/express/index.ts
+++ ch07/ch07_3_server/src/express/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import {setupRouters} from './setupRouters'
 
 export const createExpressApp = (...args: any[]) => {
   const app = express()
@@ -15,5 +16,5 @@ export const createExpressApp = (...args: any[]) => {
       res.json({message: 'Hello express World!'})
     })
 
-  return app
+  return setupRouters(app, ...args)
 }
