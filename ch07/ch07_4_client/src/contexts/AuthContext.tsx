05638b9b509e8c9338be2caf824ca74c7bf39406
kimJongWan <kimdeiz@naver.com>
2024-05-07 22:57:45 +0900

    authContext에 로그인 및 로그아웃 기능 구현

--- ch07/ch07_4_client/src/contexts/AuthContext.tsx
+++ ch07/ch07_4_client/src/contexts/AuthContext.tsx
@@ -1,52 +1,111 @@
-import type {FC, PropsWithChildren} from 'react'
-import {createContext, useCallback, useContext, useState} from 'react'
-import * as U from '../utils'
+import type { FC, PropsWithChildren } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
+import * as U from "../utils";
+import { post } from "../server";
 
-export type LoggedUser = {email: string; password: string}
-type Callback = () => void
+export type LoggedUser = { email: string; password: string };
+type Callback = () => void;
 
 type ContextType = {
-  loggedUser?: LoggedUser
-  signup: (email: string, password: string, callback?: Callback) => void
-  login: (email: string, password: string, callback?: Callback) => void
-  logout: (callback?: Callback) => void
-}
+  loggedUser?: LoggedUser;
+  signup: (email: string, password: string, callback?: Callback) => void;
+  login: (email: string, password: string, callback?: Callback) => void;
+  logout: (callback?: Callback) => void;
+};
 
 export const AuthContext = createContext<ContextType>({
   signup: (email: string, password: string, callback?: Callback) => {},
   login: (email: string, password: string, callback?: Callback) => {},
-  logout: (callback?: Callback) => {}
-})
+  logout: (callback?: Callback) => {},
+});
 
-type AuthProviderProps = {}
+type AuthProviderProps = {};
 
-export const AuthProvider: FC<PropsWithChildren<AuthProviderProps>> = ({children}) => {
-  const [loggedUser, setLoggedUser] = useState<LoggedUser | undefined>(undefined)
+export const AuthProvider: FC<PropsWithChildren<AuthProviderProps>> = ({
+  children,
+}) => {
+  const [loggedUser, setLoggedUser] = useState<LoggedUser | undefined>(
+    undefined
+  );
+  const [jwt, setJwt] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-  const signup = useCallback((email: string, password: string, callback?: Callback) => {
-    const user = {email, password}
-    setLoggedUser(notUsed => ({email, password}))
-    U.writeObjectP('user', user).finally(() => callback && callback())
-    //callback && callback()
-  }, [])
-  const login = useCallback((email: string, password: string, callback?: Callback) => {
-    setLoggedUser(notUsed => ({email, password}))
-    callback && callback()
-  }, [])
+  const signup = useCallback(
+    (email: string, password: string, callback?: Callback) => {
+      const user = { email, password };
+
+      post("/auth/signup", user)
+        .then((res) => res.json())
+        .then(
+          (result: { ok: boolean; body?: string; errorMessage?: string }) => {
+            const { ok, body, errorMessage } = result;
+            if (ok) {
+              U.writeStringP("jwt", body ?? "").finally(() => {
+                setJwt(body ?? "");
+                setLoggedUser((notUsed) => user);
+                U.writeObjectP("user", user).finally(
+                  () => callback && callback()
+                );
+              });
+            } else setErrorMessage(errorMessage ?? "");
+          }
+        )
+        .catch((e: Error) => setErrorMessage(e.message));
+    },
+    []
+  );
+  const login = useCallback(
+    (email: string, password: string, callback?: Callback) => {
+      const user = { email, password };
+      U.readStringP("jwt")
+        .then((jwt) => {
+          setJwt(jwt ?? "");
+          return post("/auth/login", user, jwt);
+        })
+        .then((res) => res.json())
+        .then((result: { ok: boolean; errorMessage?: string }) => {
+          if (result.ok) {
+            setLoggedUser((notUsed) => user);
+            callback && callback();
+          } else {
+            setErrorMessage(result.errorMessage ?? "");
+          }
+        })
+        .catch((e: Error) => setErrorMessage(e.message ?? ""));
+    },
+    []
+  );
   const logout = useCallback((callback?: Callback) => {
-    setLoggedUser(undefined)
-    callback && callback()
-  }, [])
+    setJwt((notUsed) => "");
+    setLoggedUser(undefined);
+    callback && callback();
+  }, []);
+
+  useEffect(() => {
+    U.readStringP("jwt")
+      .then((jwt) => setJwt(jwt ?? ""))
+      .catch(() => {
+        /* 오류 무시*/
+      });
+  }, []);
 
   const value = {
+    jwt,
+    errorMessage,
     loggedUser,
     signup,
     login,
-    logout
-  }
-  return <AuthContext.Provider value={value} children={children} />
-}
+    logout,
+  };
+  return <AuthContext.Provider value={value} children={children} />;
+};
 
 export const useAuth = () => {
-  return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
